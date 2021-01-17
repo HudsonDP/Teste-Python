d18636587fd1a4376a5f431ce034719551b93dcc
HudsonDP <65924763+HudsonDP@users.noreply.github.com>
2021-01-17 20:25:54 -0300

    Projeto Finalizado
    Finalizado o projeto atendendo aos requisitos passados.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Teste-Python
  
+-inicialmente foi facil extrair o conteudo html da pagina, a documentação é bem clara, e existem varios foruns sobre o tema. Porem foi identificado que o conteudo da tabela estava saparado em teams_name, e stats_table. 
+-Após coletado e tratado os valores extraidos, foi tranquilo exportar para o arquivo xls com a biblioteca xlswriter, a documentação foi bem clara e objetiva.
+-consultei varios foruns, e videos sobre web scraping para chegar a um resultado satisfatorio. 
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import collections
 import requests as r
 from bs4 import BeautifulSoup as BS4
+import xlsxwriter as xls
 
 URL = "https://gauchazh.clicrbs.com.br/esportes/tabelas/brasileirao"
 
 def main():
     html = getHTML()
     table = getTable(html=html)
-    print(table)
-
+    export_xls(table)
 
 def getHTML():
     response = r.get(URL)
@@ -18,10 +19,57 @@ def getHTML():
 def getTable(html):
     soup = BS4(html, "html.parser")
     team_table = soup.find("div", {"class":"team-table"}).find("tbody")
+    stats_table = soup.find("div", {"class":"stats-table"}).find("tbody")
     teams = team_table.find_all("td")
-    return teams
+    stats = stats_table.find_all("tr")
+    teams_list = []
+    full_table = []
 
+    for team in teams:
+        team_name = team.find("span", {"class":"team_name"})
+        teams_list.append(team_name.text)
 
+    for i in range(len(stats)):
+        stat_team = stats[i].find_all("td")
+        posicao = i+1
+        valores = {
+            "Posição":posicao,
+            "Nome do Time":teams_list[i],
+            "Numero de Pontos":stat_team[0].text,
+            "Numero de Jogos":stat_team[1].text,
+            "Numero de Vitorias":stat_team[2].text,
+            "Numero de Derrotas":stat_team[3].text,
+            "Numero de Empates":stat_team[4].text,
+            "Gols Pró":stat_team[5].text,
+            "Gols Contra":stat_team[6].text,
+            "Saldo de Gols":stat_team[7].text,
+            "Aproveitamento":stat_team[8].text,
+        }
+        full_table.append(valores)
+
+    return full_table
+
+def export_xls(full_table):
+    workbook = xls.Workbook('Tabela Brasileirao.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    col = 0
+    row = 0
+    header = full_table[0].keys()
+
+    for header_name in header:
+        worksheet.write(row, col, header_name)
+        col +=1
+
+    for i in range(len(full_table)):
+        col = 0
+        row += 1
+        values = full_table[i].values()
+        for table_values in values:
+            worksheet.write(row, col, table_values)
+            col += 1
+
+    workbook.close()
 
 if __name__ == "__main__":
     main()
